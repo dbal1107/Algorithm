011e670da8c2acd34eed07e11284fd504d2e51d8
dbal1107 <108312097+dbal1107@users.noreply.github.com>
2022-10-23 16:47:22 +0900

    Update 짝수의 합.py
    for 문 범위 설정 가능
    컴프리헨션

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
@@ -1,6 +1,9 @@
+# def solution(n):
+#     bi = list()
+#     for num in range((n//2)+1) :
+#         bi.append(2*num)
+#         ans = sum(bi)
+#     return ans
+
 def solution(n):
-    bi = list()
-    for num in range((n//2)+1) :
-        bi.append(2*num)
-        ans = sum(bi)
-    return ans
+    return sum([i for i in range(2, n + 1, 2)])
